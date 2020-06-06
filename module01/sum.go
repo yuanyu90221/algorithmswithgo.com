280215d10c64f51c1f3d524a58b38d9d8a5d1cee
jsonLiang <yuanyu90221@gmail.com>
2020-06-06 23:06:52 +0800

    recursive way of sum

--- module01/sum.go
+++ module01/sum.go
@@ -3,9 +3,18 @@ package module01
 // Sum will sum up all of the numbers passed
 // in and return the result
 func Sum(numbers []int) int {
-	sum := 0
-	for _, val := range numbers {
-		sum += val
+	// sum := 0
+	// for _, val := range numbers {
+	// 	sum += val
+	// }
+	// return sum
+	return RecusiveSum(numbers)
+}
+
+func RecusiveSum(numbers []int) int {
+	if len(numbers) == 0 {
+		return 0
+	} else {
+		return RecusiveSum(numbers[1:]) + numbers[0]
 	}
-	return sum
 }
