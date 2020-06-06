d0d3029eeb588290796b2c8d4307b196151c15ce
jsonLiang <yuanyu90221@gmail.com>
2020-06-06 22:42:51 +0800

    solve the num in list

--- module01/num_in_list.go
+++ module01/num_in_list.go
@@ -3,5 +3,12 @@ package module01
 // NumInList will return true if num is in the
 // list slice, and false otherwise.
 func NumInList(list []int, num int) bool {
+	// loop in list to check
+	// Notice first argument get from range is idx and seconde is the value
+	for _, i := range list {
+		if i == num {
+			return true
+		}
+	}
 	return false
 }
