cf2737f2a5bb128dfe5d0a92a370a0185715aa8b
jsonLiang <yuanyu90221@gmail.com>
2020-06-06 23:00:45 +0800

    solve for TestSum

--- module01/sum.go
+++ module01/sum.go
@@ -3,5 +3,9 @@ package module01
 // Sum will sum up all of the numbers passed
 // in and return the result
 func Sum(numbers []int) int {
-	return 0
+	sum := 0
+	for _, val := range numbers {
+		sum += val
+	}
+	return sum
 }
