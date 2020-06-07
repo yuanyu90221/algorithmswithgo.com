70c186f03aba005f27192879f54d9a6b37644d90
jsonLiang <yuanyu90221@gmail.com>
2020-06-07 22:49:22 +0800

    solve reverse by rume

--- module01/reverse.go
+++ module01/reverse.go
@@ -1,7 +1,5 @@
 package module01
 
-import "strings"
-
 // Reverse will return the provided word in reverse
 // order. Eg:
 //
@@ -9,15 +7,24 @@ import "strings"
 //   Reverse("alphabet") => "tebahpla"
 //
 func Reverse(word string) string {
-	result := ""
-	tokens := strings.Split(word, "")
-	length := len(tokens)
-	if length == 0 {
-		return result
-	} else {
-		for i := length - 1; i >= 0; i-- {
-			result += tokens[i]
-		}
+	// result := ""
+	// tokens := strings.Split(word, "")
+	// length := len(tokens)
+	// if length == 0 {
+	// 	return result
+	// } else {
+	// 	for i := length - 1; i >= 0; i-- {
+	// 		result += tokens[i]
+	// 	}
+	// }
+	result := ReverseWithRume(word)
+	return result
+}
+
+func ReverseWithRume(word string) string {
+	var result string
+	for _, rume := range word { // Notice character is not necessary contain 1 Bytes
+		result = string(rume) + result
 	}
 	return result
 }
