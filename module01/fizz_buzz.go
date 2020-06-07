9a2eef183440b47ef5a257a0f08e5ff5740f0435
jsonLiang <yuanyu90221@gmail.com>
2020-06-08 01:35:11 +0800

    update with separate logic of print

--- module01/fizz_buzz.go
+++ module01/fizz_buzz.go
@@ -14,30 +14,31 @@ import "fmt"
 // it here to make life easier for beginners.
 func FizzBuzz(n int) {
 
-	for i := 1; i <= n; i++ {
-		var is3times uint
-		var is5times uint
-		if i%3 == 0 {
-			is3times = 0x01
-		}
-		if i%5 == 0 {
-			is5times = 0x10
-		}
-		var judge uint = is3times | is5times
-		switch judge {
-		case 0x00:
-			if i == 1 {
-				fmt.Printf("%d", i)
-			} else {
-				fmt.Printf(", %d", i)
-			}
-		case 0x01:
-			fmt.Printf(", Fizz")
-		case 0x10:
-			fmt.Printf(", Buzz")
-		case 0x11:
-			fmt.Printf(", Fizz Buzz")
-		}
+	for i := 1; i < n; i++ {
+		PrintFizzBuzzValue(i)
+		fmt.Print(", ")
 	}
+	PrintFizzBuzzValue(n)
 	fmt.Println()
+}
+func PrintFizzBuzzValue(i int) {
+	var is3times uint
+	var is5times uint
+	if i%3 == 0 {
+		is3times = 0x01
+	}
+	if i%5 == 0 {
+		is5times = 0x10
+	}
+	var judge uint = is3times | is5times
+	switch judge {
+	case 0x00:
+		fmt.Printf("%d", i)
+	case 0x01:
+		fmt.Printf("Fizz")
+	case 0x10:
+		fmt.Printf("Buzz")
+	case 0x11:
+		fmt.Printf("Fizz Buzz")
+	}
 }
