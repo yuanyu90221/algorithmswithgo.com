c484fb4d39cf1e76b3f318b1a5e56a1f44016528
jsonLiang <yuanyu90221@gmail.com>
2020-06-09 01:47:20 +0800

    solve BaseToDec

--- module01/base_to_dec.go
+++ module01/base_to_dec.go
@@ -1,5 +1,10 @@
 package module01
 
+import (
+	"math"
+	"strconv"
+)
+
 // BaseToDec takes in a number and the base it is currently
 // in and returns the decimal equivalent as an integer.
 //
@@ -9,5 +14,30 @@ package module01
 //   BaseToDec("1110", 2) => 14
 //
 func BaseToDec(value string, base int) int {
-	return 0
+	length := len(value)
+	result := 0
+	for idx, value := range value {
+		result += ConvertDigit(value, base, length-idx-1)
+	}
+	return result
+}
+
+func ConvertDigit(digit rune, base int, index int) int {
+	switch digit {
+	case 'A':
+		return int(math.Pow(float64(base), float64(index))) * 10
+	case 'B':
+		return int(math.Pow(float64(base), float64(index))) * 11
+	case 'C':
+		return int(math.Pow(float64(base), float64(index))) * 12
+	case 'D':
+		return int(math.Pow(float64(base), float64(index))) * 13
+	case 'E':
+		return int(math.Pow(float64(base), float64(index))) * 14
+	case 'F':
+		return int(math.Pow(float64(base), float64(index))) * 15
+	default:
+		result, _ := strconv.Atoi(string(digit))
+		return int(math.Pow(float64(base), float64(index))) * result
+	}
 }
