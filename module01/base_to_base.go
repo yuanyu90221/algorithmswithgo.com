e68ce07897a2db1619040de6d237fd021f46eba5
jsonLiang <yuanyu90221@gmail.com>
2020-06-10 02:26:38 +0800

    resolve baseTobase

--- module01/base_to_base.go
+++ module01/base_to_base.go
@@ -1,5 +1,7 @@
 package module01
 
+import "fmt"
+
 // BaseToBase takes in a number, the base it is currently
 // in, and the base you want it to be converted to. It then
 // returns a string representing the number in the new base.
@@ -9,5 +11,27 @@ package module01
 //   BaseToBase("E", 16, 2) => "1110"
 //
 func BaseToBase(value string, base, newBase int) string {
-	return ""
+	return _DecToBase(_BaseToDec(value, base), newBase)
+}
+
+func _BaseToDec(value string, base int) int {
+	result := 0
+	multiplier := 1
+	for i := len(value) - 1; i >= 0; i-- {
+		var digit int
+		fmt.Sscanf(string(value[i]), "%X", &digit)
+		result += digit * multiplier
+		multiplier *= base
+	}
+	return result
+}
+
+func _DecToBase(dec int, base int) string {
+	result := ""
+	for dec > 0 {
+		remain := dec % base
+		result = fmt.Sprintf("%X%s", remain, result)
+		dec = dec / base
+	}
+	return result
 }
